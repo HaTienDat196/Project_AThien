336a06f4271b6ceee3f3fbaa0ca8d19e7b0fa7bd
TienDat <hatiendat0803@gmail.com>
2018-08-19 01:31:08 +0700

    confix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  as :user do
+    get 'signin' => 'devise/sessions#new'
+    post 'signin' => 'devise/sessions#create'
+    delete 'signout' => 'devise/sessions#destroy'
+  end
   get 'users/index'
   devise_for :users
   root to: "users#index"
