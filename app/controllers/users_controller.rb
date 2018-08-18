9207a3ca5c70015e76696e8e2cea121182c2aa30
TienDat <hatiendat0803@gmail.com>
2018-08-19 00:41:05 +0700

    complete display and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,24 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new]
   def index
   end
-  def new
 
+  def show
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
