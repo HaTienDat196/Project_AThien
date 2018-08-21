c85afe85bdfc2bc2e59b31c9612c7c930a17d46e
TienDat <hatiendat0803@gmail.com>
2018-08-21 16:45:58 +0700

    login facebook-gmail

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index' => 'users/index'
+  root to: "users#show"
+  resource :user
+
   as :user do
     get 'signin' => 'devise/sessions#new'
     post 'signin' => 'devise/sessions#create'
     delete 'signout' => 'devise/sessions#destroy'
-    get 'user' => 'users/show'
   end
-  get 'users/index' => 'users/index'
-  devise_for :users
-  root to: "users#show"
-  resource :user
+
+ devise_for :users,
+    controllers:{omniauth_callbacks: "users/omniauth_callbacks"}
 end
